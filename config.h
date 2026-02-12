afa599dde19d6da5c26d306dbeb4b7ed7c257b20
Andershell <andershell@synframe.eu>
2026-02-12 23:46:56 +0100

    sync

--- config.h
+++ config.h
@@ -5,15 +5,14 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka:pixelsize=13:antialias=true:autohint=true";
+static char *font = "Iosevka:pixelsize=26:antialias=true:autohint=true";
 // static char *font = "JetBrains Mono:pixelsize=13:antialias=true:autohint=true";
 // static char *font = "Liberation Mono:pixelsize=11:antialias=true:autohint=true";
 // static char *font = "mono:pixelsize=11:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-    // "Inconsolata for Powerline:pixelsize=13:antialias=true:autohint=true",
-    "Hack Nerd Font Mono:pixelsize=13:antialias=true:autohint=true",
-    "NotoColorEmoji:pixelsize=13:antialias=true:autohint=true",
+    "Hack Nerd Font Mono:pixelsize=25:antialias=true:autohint=true",
+    "NotoColorEmoji:pixelsize=25:antialias=true:autohint=true",
 };
 
 static int borderpx = 1;
